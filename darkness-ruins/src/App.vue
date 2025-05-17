8dd7abbec930b0d4ef1fba562bede54a5b6e6b64
Pietra Rolim Mendes <pietra2004@gmail.com>
2025-05-17 00:08:45 -0300

    Projeto Darkness Ruins com Vue e Phaser

---
+++ darkness-ruins/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+ <img src="./assets/sprites/hero.png"> 
+</template>
+
+<style scoped>
+
+</style>
